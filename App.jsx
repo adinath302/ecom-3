15756bcaca455455a94084a6dd852b7fda5161df
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2024-12-19 21:59:58 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+// import App from './App.css'
+
+const App = () => {
+  return (
+    <div className="">
+      <Navbar />
+      <Hero />
+    </div>
+  );
+};
+
+export default App;
